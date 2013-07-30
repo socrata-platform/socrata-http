a58ce891fa1af143cd8ceb819349e81bbba0025d
Robert J. Macomber <robert.macomber@socrata.com>
2013-07-30 14:26:11 -0700

    Start 2.0; drop Scala <2.10 support in preparation for a big reorg

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -7,9 +7,8 @@ import com.typesafe.tools.mima.plugin.MimaKeys.previousArtifact
 object BuildSettings {
   val buildSettings: Seq[Setting[_]] = Defaults.defaultSettings ++ Seq(
     organization := "com.socrata",
-    version := "1.3.1-SNAPSHOT",
-    scalaVersion := "2.10.0",
-    crossScalaVersions := Seq("2.8.1", "2.9.2", "2.10.0")
+    version := "2.0.0-SNAPSHOT",
+    scalaVersion := "2.10.0"
   )
 
   val projectSettings: Seq[Setting[_]] = buildSettings ++ mimaDefaultSettings ++ Seq(
@@ -17,18 +16,11 @@ object BuildSettings {
     testOptions in Test ++= Seq(
       Tests.Argument(TestFrameworks.ScalaTest, "-oFD")
     ),
-    libraryDependencies <+= (scalaVersion) {
-      case "2.8.1" => "org.scalatest" % "scalatest_2.8.1" % "1.8" % "test"
-      case _ => "org.scalatest" %% "scalatest" % "1.9.1" % "test"
-    },
-    scalacOptions <++= (scalaVersion) map {
-      case s if s.startsWith("2.8.") => Seq("-encoding", "UTF-8", "-g", "-unchecked", "-deprecation")
-      case s if s.startsWith("2.9.") => Seq("-encoding", "UTF-8", "-g:vars", "-unchecked", "-deprecation")
-      case s if s.startsWith("2.10.") => Seq("-encoding", "UTF-8", "-g:vars", "-deprecation", "-feature", "-language:implicitConversions")
-    },
+    libraryDependencies ++= Seq(
+      "org.scalatest" %% "scalatest" % "1.9.1" % "test"
+    ),
+    scalacOptions ++= Seq("-encoding", "UTF-8", "-g:vars", "-deprecation", "-feature", "-language:implicitConversions"),
     javacOptions ++= Seq("-encoding", "UTF-8", "-g", "-Xlint:unchecked", "-Xlint:deprecation", "-Xmaxwarns", "999999"),
-    unmanagedSourceDirectories in Compile <++= (scalaVersion, scalaSource in Compile)(versionSpecificSourceDir),
-    unmanagedSourceDirectories in Test <++= (scalaVersion, scalaSource in Test)(versionSpecificSourceDir),
     ivyXML := // com.rojoma and com.socrata have binary compat guarantees
       <dependencies>
         <conflict org="com.socrata" manager="latest-compatible"/>
@@ -37,9 +29,4 @@ object BuildSettings {
   )
 
   val slf4jVersion = "1.7.5"
-
-  def versionSpecificSourceDir(scalaVersion: String, scalaSource: File) = scalaVersion match {
-    case s if s.startsWith("2.10.") => Seq(scalaSource.getParentFile / "scala-2.10")
-    case _ => Nil
-  }
 }
