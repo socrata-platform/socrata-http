2281b7f8eb72d1d300d14be73a6d746913b782f1
Robert J. Macomber <robert.macomber@socrata.com>
2013-08-16 14:50:37 -0700

    AcknowledgeableReader
    Like AcknowledgeableInputStream, only a Reader.

---
+++ socrata-http-common/src/main/scala/com/socrata/http/common/util/AcknowledgeableReader.scala
@@ -0,0 +1,33 @@
+package com.socrata.http.common.util
+
+import java.io.Reader
+
+class AcknowledgeableReader(underlying: Reader, limit: Long) extends Reader with Acknowledgeable {
+  private var readSoFar: Long = 0L
+
+  private def checkSize() {
+    if(readSoFar > limit || readSoFar < 0) throw new TooMuchDataWithoutAcknowledgement
+  }
+
+  def acknowledge() {
+    readSoFar = 0L
+  }
+
+  override def read(): Int = {
+    checkSize()
+    val result = underlying.read()
+    if(result >= 0) readSoFar += 1
+    result
+  }
+
+  def read(cbuf: Array[Char], off: Int, len: Int): Int = {
+    checkSize()
+    val result = underlying.read(cbuf, off, len)
+    if(result >= 0) readSoFar += result
+    result
+  }
+
+  def close() {
+    underlying.close()
+  }
+}
