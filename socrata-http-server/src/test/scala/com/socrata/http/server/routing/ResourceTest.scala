023598d400dc2d99c0eb48ca8515358e31760acc
Robert J. Macomber <robert.macomber@socrata.com>
2013-08-20 10:46:54 -0700

    Rename inner classes in ResourceTest
    Because of a case-sensitivity warning about the synthetic
    "Foo$class" generated by scalac.

--- socrata-http-server/src/test/scala/com/socrata/http/server/routing/ResourceTest.scala
+++ socrata-http-server/src/test/scala/com/socrata/http/server/routing/ResourceTest.scala
@@ -14,31 +14,31 @@ object TopLevelSingletonResourceWithGetAndPost extends SimpleResource {
 }
 
 object ResourceInsideASingleton {
-  object Singleton extends SimpleResource {
+  object SingletonR extends SimpleResource {
     override def delete = ???
   }
 
-  class Class extends SimpleResource {
+  class ClassR extends SimpleResource {
     override def patch = ???
   }
 }
 
 class ResourceInsideAClass {
-  object Singleton extends SimpleResource {
+  object SingletonR extends SimpleResource {
     override def delete = ???
   }
 
-  class Class extends SimpleResource {
+  class ClassR extends SimpleResource {
     override def patch = ???
   }
 }
 
 trait ResourceInsideATrait {
-  object Singleton extends SimpleResource {
+  object SingletonR extends SimpleResource {
     override def delete = ???
   }
 
-  class Class extends SimpleResource {
+  class ClassR extends SimpleResource {
     override def patch = ???
   }
 }
@@ -53,20 +53,20 @@ class ResourceTest extends FunSuite with MustMatchers {
   }
 
   test("Resources nested within a top-level object compute their allowed-method sets correctly") {
-    ResourceInsideASingleton.Singleton.allowedMethods must equal (Set("DELETE"))
-    (new ResourceInsideASingleton.Class).allowedMethods must equal (Set("PATCH"))
+    ResourceInsideASingleton.SingletonR.allowedMethods must equal (Set("DELETE"))
+    (new ResourceInsideASingleton.ClassR).allowedMethods must equal (Set("PATCH"))
   }
 
   test("Resources nested within a top-level class compute their allowed-method sets correctly") {
     val c = new ResourceInsideAClass
-    c.Singleton.allowedMethods must equal (Set("DELETE"))
-    (new c.Class).allowedMethods must equal (Set("PATCH"))
+    c.SingletonR.allowedMethods must equal (Set("DELETE"))
+    (new c.ClassR).allowedMethods must equal (Set("PATCH"))
   }
 
   test("Resources nested within a top-level trait compute their allowed-method sets correctly") {
     val c = new ResourceInsideATrait {}
-    c.Singleton.allowedMethods must equal (Set("DELETE"))
-    (new c.Class).allowedMethods must equal (Set("PATCH"))
+    c.SingletonR.allowedMethods must equal (Set("DELETE"))
+    (new c.ClassR).allowedMethods must equal (Set("PATCH"))
   }
 
   test("Anonymous resources compute their allowed-method sets correctly") {
