a4dec88a2ffe1e46d011e91f4c481d71e5890d13
Robert J. Macomber <robert.macomber@socrata.com>
2013-08-20 11:26:35 -0700

    Simplify "hostname" method on HttpServletRequest augmentation

--- socrata-http-server/src/main/scala/com/socrata/http/server/implicits.scala
+++ socrata-http-server/src/main/scala/com/socrata/http/server/implicits.scala
@@ -18,8 +18,7 @@ object implicits {
 
   implicit class PimpedOutRequest(val underlying: HttpServletRequest) extends AnyVal {
     def hostname =
-      Option(underlying.getHeader("X-Socrata-Host")).getOrElse(
-        Option(underlying.getHeader("Host")).getOrElse("")).split(':').head
+      header("X-Socrata-Host").orElse(header("Host")).getOrElse("").split(':').head
 
     def requestPath: List[String] = // TODO: strip off any context and/or servlet path
       underlying.getRequestURI.split("/", -1 /* I hate you, Java */).iterator.drop(1).map(URLDecoder.decode(_, "UTF-8")).toList
