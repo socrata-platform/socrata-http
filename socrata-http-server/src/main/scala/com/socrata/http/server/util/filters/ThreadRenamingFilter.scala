3b88cd295d0d8ef1db005ba138b7b4760c5fe478
Robert J. Macomber <robert.macomber@socrata.com>
2013-08-26 15:48:06 -0700

    socrata-server-jetty: Filter to set thread name for request

---
+++ socrata-http-server/src/main/scala/com/socrata/http/server/util/filters/ThreadRenamingFilter.scala
@@ -0,0 +1,26 @@
+package com.socrata.http.server.util.filters
+
+import com.socrata.http.server.{Filter, HttpResponse}
+import javax.servlet.http.HttpServletRequest
+
+abstract class ThreadRenamingFilter[InDown, OutUp] extends Filter[InDown, OutUp, InDown, OutUp] {
+  def apply(request: InDown, service: (InDown) => OutUp): OutUp = {
+    val me = Thread.currentThread
+    val oldName = me.getName
+    try {
+      me.setName(newName(request))
+      service(request)
+    } finally {
+      me.setName(oldName)
+    }
+  }
+
+  def newName(in: InDown): String
+}
+
+object ThreadRenamingFilter {
+  object Default extends ThreadRenamingFilter[HttpServletRequest, HttpResponse] {
+    def newName(req: HttpServletRequest) =
+      Thread.currentThread.getId + " / " + req.getMethod + " " + req.getRequestURI + Option(req.getQueryString).fold("")("?" + _)
+  }
+}
