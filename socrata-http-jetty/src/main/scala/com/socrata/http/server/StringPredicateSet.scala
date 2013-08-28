0a7803364e248af989bc467edaa487cdac7f4334
Robert J. Macomber <robert.macomber@socrata.com>
2013-08-28 13:57:55 -0700

    Finish a comment

--- socrata-http-jetty/src/main/scala/com/socrata/http/server/StringPredicateSet.scala
+++ socrata-http-jetty/src/main/scala/com/socrata/http/server/StringPredicateSet.scala
@@ -1,7 +1,8 @@
 package com.socrata.http.server
 
 // I am a dirty liar.
-// This is for dealing with Java code that takes
+// This is for dealing with Java code that takes a Set<String> when it really wants
+// a predicate on strings.
 private class StringPredicateSet(f: String => Boolean) extends java.util.AbstractSet[String] {
   override def contains(s: AnyRef) = { s.isInstanceOf[String] && f(s.asInstanceOf[String]) }
   def size(): Int = ???
