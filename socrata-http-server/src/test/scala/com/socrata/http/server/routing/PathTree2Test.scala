25fe34807c64f2deaeb9174c8f074a5143992976
Robert J. Macomber <robert.macomber@socrata.com>
2013-08-30 15:29:59 -0700

    Another test to make sure extractors are being merged correctly.

--- socrata-http-server/src/test/scala/com/socrata/http/server/routing/PathTree2Test.scala
+++ socrata-http-server/src/test/scala/com/socrata/http/server/routing/PathTree2Test.scala
@@ -15,6 +15,10 @@ class PathTree2Test extends FunSuite with MustMatchers with PropertyChecks {
     PathTree2(xs.map { x => new Matcher { def matches(s: String) = s == x; override def toString = com.rojoma.json.ast.JString(x).toString } }, f, wantMore)
   }
 
+  def sm(s: String) = new Matcher.StringMatcher(s)
+  def es = Extractor[String]
+  def ei = Extractor[Int]
+
   test("A literal pathtree that doesn't want more matches itself") {
     forAll { (xs: List[String]) =>
       whenever(xs.nonEmpty) {
@@ -133,14 +137,31 @@ class PathTree2Test extends FunSuite with MustMatchers with PropertyChecks {
   }
 
   test("An extracting match returns the found objects in the order in which they were extracted") {
-    def sm(s: String) = new Matcher.StringMatcher(s)
-    def es = Extractor[String]
-    def ei = Extractor[Int]
-
     val r1 = PathTree2(List(sm("hello"), es, ei, sm("world")), { xs: List[Any] => "extracted" :: xs }, flex = false)
     r1(List("hello","there","42","world")) must equal (Some(List("extracted", "there", 42)))
 
     val r2 = PathTree2(List(sm("hello"), es, ei, sm("world")), { xs: List[Any] => "extracted" :: xs }, flex = true)
     r2(List("hello","there","42","world","gnu")) must equal (Some(List("extracted", "there", 42, List("gnu"))))
+  }
+
+  test("adjacent identical extractors merge") {
+    val r1 = PathTree2(List(sm("hello"), es, sm("world")), "r1", flex = false)
+    val r2 = PathTree2(List(sm("hello"), es, sm("there")), "r2", flex = false)
+    val r3 = PathTree2(List(sm("hello"), ei), "r3", flex = false)
+
+    (r1 merge r2 merge r3) must equal (
+      LiteralOnlyPathTree2(
+        Map(
+          "hello" -> MatchingPathTree2(
+            List(
+              es -> LiteralOnlyPathTree2(
+                Map(
+                  "world" -> LiteralOnlyPathTree2(Map.empty, Some("r1"), None),
+                  "there" -> LiteralOnlyPathTree2(Map.empty, Some("r2"), None)),
+                None, None),
+              ei -> LiteralOnlyPathTree2(Map.empty, Some("r3"), None)),
+            None, None)),
+        None, None)
+    )
   }
 }
