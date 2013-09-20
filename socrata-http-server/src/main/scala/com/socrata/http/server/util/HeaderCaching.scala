a03232acac74c2d820483231c4a67b7f43461b97
Robert J. Macomber <robert.macomber@socrata.com>
2013-09-20 14:11:46 -0700

    Rename Precondition#passes to #check

--- socrata-http-server/src/main/scala/com/socrata/http/server/util/HeaderCaching.scala
+++ socrata-http-server/src/main/scala/com/socrata/http/server/util/HeaderCaching.scala
@@ -63,22 +63,25 @@ object EntityTag {
 }
 
 sealed abstract class Precondition {
-  def passes(tag: Option[EntityTag], sideEffectFree: Boolean): Precondition.Result
+  @deprecated("Use `check' instead", since = "2.0.0")
+  def passes(tag: Option[EntityTag], sideEffectFree: Boolean): Precondition.Result = check(tag, sideEffectFree)
+  def check(tag: Option[EntityTag], sideEffectFree: Boolean): Precondition.Result
+
   def filter(f: EntityTag => Boolean): Either[Precondition.Failure, Precondition]
   def map(f: EntityTag => EntityTag): Precondition
 }
 case object NoPrecondition extends Precondition {
-  def passes(tag: Option[EntityTag], sideEffectFree: Boolean) = Precondition.Passed
+  def check(tag: Option[EntityTag], sideEffectFree: Boolean) = Precondition.Passed
   def filter(f: EntityTag => Boolean) = Right(this)
   def map(f: EntityTag => EntityTag) = this
 }
 case object IfDoesNotExist extends Precondition {
-  def passes(tag: Option[EntityTag], sideEffectFree: Boolean) = if(tag.nonEmpty) Precondition.FailedBecauseMatch else Precondition.Passed
+  def check(tag: Option[EntityTag], sideEffectFree: Boolean) = if(tag.nonEmpty) Precondition.FailedBecauseMatch else Precondition.Passed
   def filter(f: EntityTag => Boolean) = Right(this)
   def map(f: EntityTag => EntityTag) = this
 }
 case class IfNoneOf(etag: Seq[EntityTag]) extends Precondition {
-  def passes(tag: Option[EntityTag], sideEffectFree: Boolean) = tag match {
+  def check(tag: Option[EntityTag], sideEffectFree: Boolean) = tag match {
     case Some(t) =>
       val matches =
         if(sideEffectFree) etag.exists(_.weakCompare(t))
@@ -96,12 +99,12 @@ case class IfNoneOf(etag: Seq[EntityTag]) extends Precondition {
   def map(f: EntityTag => EntityTag) = IfNoneOf(etag.map(f))
 }
 case object IfExists extends Precondition {
-  def passes(tag: Option[EntityTag], sideEffectFree: Boolean) = if(tag.isEmpty) Precondition.FailedBecauseNoMatch else Precondition.Passed
+  def check(tag: Option[EntityTag], sideEffectFree: Boolean) = if(tag.isEmpty) Precondition.FailedBecauseNoMatch else Precondition.Passed
   def filter(f: EntityTag => Boolean) = Right(this)
   def map(f: EntityTag => EntityTag) = this
 }
 case class IfAnyOf(etag: Seq[EntityTag]) extends Precondition {
-  def passes(tag: Option[EntityTag], sideEffectFree: Boolean) = tag match {
+  def check(tag: Option[EntityTag], sideEffectFree: Boolean) = tag match {
     case Some(t) =>
       if(etag.exists(_.strongCompare(t))) Precondition.Passed
       else Precondition.FailedBecauseNoMatch
@@ -116,10 +119,10 @@ case class IfAnyOf(etag: Seq[EntityTag]) extends Precondition {
   }
 }
 case class AndPrecondition(a: Precondition, b: Precondition) extends Precondition {
-  def passes(tag: Option[EntityTag], sideEffectFree: Boolean) =
-    a.passes(tag, sideEffectFree) match {
+  def check(tag: Option[EntityTag], sideEffectFree: Boolean) =
+    a.check(tag, sideEffectFree) match {
       case f: Precondition.Failure => f
-      case Precondition.Passed => b.passes(tag, sideEffectFree)
+      case Precondition.Passed => b.check(tag, sideEffectFree)
     }
 
   def map(f: (EntityTag) => EntityTag): Precondition =
