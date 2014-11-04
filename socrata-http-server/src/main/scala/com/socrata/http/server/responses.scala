64ac4818de5a8d3f7a2cd92918d381853f8366d3
Robert J. Macomber <robert.macomber@socrata.com>
2014-11-04 14:28:25 -0800

    Bunch of extra HTTP statuses

--- socrata-http-server/src/main/scala/com/socrata/http/server/responses.scala
+++ socrata-http-server/src/main/scala/com/socrata/http/server/responses.scala
@@ -70,32 +70,49 @@ object responses {
   val Accepted = Status(HttpServletResponse.SC_ACCEPTED)
   val Created = Status(HttpServletResponse.SC_CREATED)
   val NoContent = Status(HttpServletResponse.SC_NO_CONTENT)
+  val PartialContent = Status(HttpServletResponse.SC_PARTIAL_CONTENT)
 
   // 3xx
-  def MovedTemporarily(url: URL) = Status(HttpServletResponse.SC_MOVED_TEMPORARILY) ~> Location(url)
+  val MultipleChoices = Status(HttpServletResponse.SC_MULTIPLE_CHOICES)
   def MovedPermanently(url: URL) = Status(HttpServletResponse.SC_MOVED_PERMANENTLY) ~> Location(url)
+  def Found(url: URL) = Status(HttpServletResponse.SC_FOUND) ~> Location(url)
+  def MovedTemporarily(url: URL) = Status(HttpServletResponse.SC_MOVED_TEMPORARILY) ~> Location(url)
   def SeeOther(url: URL) = Status(HttpServletResponse.SC_SEE_OTHER) ~> Location(url)
   val NotModified = Status(HttpServletResponse.SC_NOT_MODIFIED)
   def TemporaryRedirect(url: URL) = Status(HttpServletResponse.SC_TEMPORARY_REDIRECT) ~> Location(url)
 
   // 4xx
+  val BadRequest = Status(HttpServletResponse.SC_BAD_REQUEST)
   val Unauthorized = Status(HttpServletResponse.SC_UNAUTHORIZED)
+  val PaymentRequired = Status(HttpServletResponse.SC_PAYMENT_REQUIRED)
   val Forbidden = Status(HttpServletResponse.SC_FORBIDDEN)
   val NotFound = Status(HttpServletResponse.SC_NOT_FOUND)
-  val BadRequest = Status(HttpServletResponse.SC_BAD_REQUEST)
   val MethodNotAllowed = Status(HttpServletResponse.SC_METHOD_NOT_ALLOWED)
   val NotAcceptable = Status(HttpServletResponse.SC_NOT_ACCEPTABLE) // Can't fulfil client's request for a content-type
+  val RequestTimeout = Status(HttpServletResponse.SC_REQUEST_TIMEOUT) // Client took too long
   val Conflict = Status(HttpServletResponse.SC_CONFLICT) // Someone else is making a change at the same time (i.e., lock acquisition failure)
+  val Gone = Status(HttpServletResponse.SC_GONE) // Like 404 only it used to be here
+  val LengthRequired = Status(HttpServletResponse.SC_LENGTH_REQUIRED) // Needs a content-length
   val PreconditionFailed = Status(HttpServletResponse.SC_PRECONDITION_FAILED)
   val RequestEntityTooLarge = Status(HttpServletResponse.SC_REQUEST_ENTITY_TOO_LARGE)
+  val RequestURITooLong = Status(HttpServletResponse.SC_REQUEST_URI_TOO_LONG)
   val UnsupportedMediaType = Status(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE) // upload in a format we don't understand
+  val RequestedRangeNotSatisfiable = Status(HttpServletResponse.SC_REQUESTED_RANGE_NOT_SATISFIABLE)
+  val ExpectationFailed = Status(HttpServletResponse.SC_EXPECTATION_FAILED)
   val ImATeapot = Status(418) // What?  HttpServletResponse doesn't pre-define this one?
+  val PreconditionRequired = Status(428) // RFC 6585
+  val TooManyRequests = Status(429) // RFC 6585
+  val RequestHeaderFieldsTooLarge = Status(431) // RFC 6585
+  val UnavailableForLegalReasons = Status(451)
 
   // 5xx
   val InternalServerError = Status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR)
+  val NotImplemented = Status(HttpServletResponse.SC_NOT_IMPLEMENTED)
+  val BadGateway = Status(HttpServletResponse.SC_BAD_GATEWAY)
   val ServiceUnavailable = Status(HttpServletResponse.SC_SERVICE_UNAVAILABLE)
   val GatewayTimeout = Status(HttpServletResponse.SC_GATEWAY_TIMEOUT)
   val HTTPVersionNotSupported = Status(HttpServletResponse.SC_HTTP_VERSION_NOT_SUPPORTED)
+  val VariantAlsoNegotiates = Status(506) // RFC 2295
 
   private def r(mutator: HttpServletResponse => Unit) = mutator
 }
