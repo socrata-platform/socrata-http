5170de04c65168cf64f101839be846da6a6b919e
Evan Chan <evan.chan@socrata.com>
2014-11-07 14:32:23 -0800

    Set MDC for X-Socrata-RequestId if we generate one

--- socrata-http-server/src/main/scala/com/socrata/http/server/util/RequestId.scala
+++ socrata-http-server/src/main/scala/com/socrata/http/server/util/RequestId.scala
@@ -1,6 +1,7 @@
 package com.socrata.http.server.util
 
 import javax.servlet.http.HttpServletRequest
+import org.slf4j.MDC
 
 /**
  * Utilities for obtaining a universal request ID for socrata-http apps.
@@ -13,15 +14,22 @@ object RequestId {
 
   /**
    * Obtains a RequestId from an HTTP request, generating one if not present.
+   * Also inserts the RequestId into MDC for logging if not present.
    * @param req the [[HttpServletRequest]] object
    * @returns the RequestId from the request or a generated one
    */
   def getFromRequest(req: HttpServletRequest): RequestId =
-    Option(req.getHeader(ReqIdHeader)).getOrElse(generate())
+    Option(req.getHeader(ReqIdHeader)).getOrElse(generateAndPut())
 
   /**
    * Generates a random Request ID.
    * NOTE: this is probably good enough but should we consider using UUIDs?
    */
   def generate(): RequestId = math.BigInt(128, secureRandom).toString(36)
+
+  private def generateAndPut(): RequestId = {
+    val reqId = generate()
+    MDC.put(ReqIdHeader, reqId)
+    reqId
+  }
 }
