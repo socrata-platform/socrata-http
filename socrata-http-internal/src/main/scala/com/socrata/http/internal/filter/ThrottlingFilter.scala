f9d92bf452e9d58a7822488e61bb48c6fb141e0e
Marc Millstone <marc.millstone@socrata.com>
2015-02-12 11:20:40 -0800

    Integrate CMS with ThrottlingFilter

--- socrata-http-internal/src/main/scala/com/socrata/http/internal/filter/ThrottlingFilter.scala
+++ socrata-http-internal/src/main/scala/com/socrata/http/internal/filter/ThrottlingFilter.scala
@@ -2,6 +2,7 @@ package com.socrata.http.internal.filter
 
 import javax.servlet.http.HttpServletRequest
 
+import com.socrata.http.internal.Throttler
 import com.socrata.http.server.{HttpService, HttpResponse, SimpleFilter}
 import com.socrata.http.server.implicits._
 import com.socrata.http.server.responses._
@@ -9,13 +10,19 @@ import com.socrata.http.server.implicits._
 
 
 
-class ThrottlingFilter() extends SimpleFilter[HttpServletRequest, HttpResponse] {
+class ThrottlingFilter(throttler:Throttler) extends SimpleFilter[HttpServletRequest, HttpResponse] {
   override def apply(req:HttpServletRequest, service:HttpService) = {
     val token = req.header("X-App-token")
     token match  {
-      case Some(tok) => ???
+      case Some(tok) =>
+        if(throttler.permitAccess(tok))
+          service(req)
+        else
+          Forbidden ~> ContentType("application/json") ~> Content(""" {"message":"You api access is currently being throttled to this instance"} """)
 
-      case None => Forbidden ~> ContentType("application/json") ~> Content("""{ "message":"Please specify an app token"}""")
+      case None =>
+        // TODO:  Add parsing on tokens on query string
+        Forbidden ~> ContentType("application/json") ~> Content("""{ "message":"Please specify an app token"}""")
     }
   }
 
