f1472085190f9043cb7fa30412c5cb247f217f22
Chi Tang <chi.tang@socrata.com>
2015-06-01 12:31:23 -0700

    Revert "Merge pull request #27 from socrata-platform/lazy-port"
    This reverts commit 554824d91483bbf285232f939ed1b728b4f907a5, reversing
    changes made to 66e0af367d13fef40b09a4f53b2a6a03f3578510.

--- socrata-http-server/src/main/scala/com/socrata/http/server/livenesscheck/LivenessCheckConfig.scala
+++ socrata-http-server/src/main/scala/com/socrata/http/server/livenesscheck/LivenessCheckConfig.scala
@@ -5,7 +5,7 @@ import com.typesafe.config.Config
 
 class LivenessCheckConfig(config: Config, root: String) extends ConfigClass(config, root) {
   /** Listen on port if specified, otherwise use ephemeral port. */
-  lazy val port = optionally(getInt("port"))
+  val port = optionally(getInt("port"))
   /** Bind to address of the specific hostname or IP if specified, otherwise use wildcard. This should be set on
     * systems with multiple interfaces on the same network or you may risk sending responses from the wrong IP.
     */
