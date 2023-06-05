a1505b34a708c2bbf1ecf97abba90f760f982145
Robert J. Macomber <robert.macomber@socrata.com>
2023-06-05 09:30:28 -0700

    Add a bunch of predefined headers

--- socrata-http-server-ext/src/main/scala/com/socrata/http/server/ext/HeaderName.scala
+++ socrata-http-server-ext/src/main/scala/com/socrata/http/server/ext/HeaderName.scala
@@ -37,4 +37,38 @@ object HeaderName {
     require(validHeader(s))
     new HeaderName(s.toLowerCase)
   }
+
+  def intern(s: String) = {
+    require(validHeader(s))
+    new HeaderName(s.toLowerCase.intern())
+  }
+
+  val Authorization = HeaderName.intern("authorization")
+  val Age = HeaderName.intern("age")
+  val CacheControl = HeaderName.intern("cache-control")
+  val Expires = HeaderName.intern("expires")
+  val Pragma = HeaderName.intern("pragma")
+  val LastModified = HeaderName.intern("last-modified")
+  val ETag = HeaderName.intern("etag")
+  val IfMatch = HeaderName.intern("if-match")
+  val IfNoneMatch = HeaderName.intern("if-none-match")
+  val IfModifiedSince = HeaderName.intern("if-modified-since")
+  val IfUnmodifiedSince = HeaderName.intern("if-unmodified-since")
+  val Vary = HeaderName.intern("vary")
+  val Accept = HeaderName.intern("accept")
+  val AcceptEncoding = HeaderName.intern("accept-encoding")
+  val AcceptLanguage = HeaderName.intern("accept-language")
+  val Expect = HeaderName.intern("expect")
+  val Cookie = HeaderName.intern("cookie")
+  val SetCookie = HeaderName.intern("set-cookie")
+  val ContentDisposition = HeaderName.intern("content-disposition")
+  val ContentLength = HeaderName.intern("content-length")
+  val ContentType = HeaderName.intern("content-type")
+  val ContentEncoding = HeaderName.intern("content-encoding")
+  val ContentLanguage = HeaderName.intern("content-language")
+  val ContentLocation = HeaderName.intern("content-location")
+  val Location = HeaderName.intern("location")
+  val Host = HeaderName.intern("host")
+  val Referer = HeaderName.intern("referer")
+  val Allow = HeaderName.intern("allow")
 }
