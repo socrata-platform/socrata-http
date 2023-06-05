958f9c14b254c9b85107f093c4db9d6ec34e3664
Robert J. Macomber <robert.macomber@socrata.com>
2023-06-05 09:22:48 -0700

    Add etag intoresponseparts impl

--- socrata-http-server-ext/src/main/scala/com/socrata/http/server/ext/IntoResponseParts.scala
+++ socrata-http-server-ext/src/main/scala/com/socrata/http/server/ext/IntoResponseParts.scala
@@ -1,5 +1,7 @@
 package com.socrata.http.server.ext
 
+import com.socrata.http.server.util.{EntityTag, EntityTagRenderer}
+
 trait IntoResponseParts[R] {
   def intoResponseParts(r: R, parts: ResponseParts): ResponseParts
 }
@@ -7,4 +9,12 @@ trait IntoResponseParts[R] {
 object IntoResponseParts {
   def apply[R](r: R, parts: ResponseParts)(implicit ev: IntoResponseParts[R]) =
     ev.intoResponseParts(r, parts)
+
+  implicit object entityTag extends IntoResponseParts[EntityTag] {
+    private val etagHeader = HeaderName("etag")
+
+    def intoResponseParts(etag: EntityTag, parts: ResponseParts): ResponseParts = {
+      parts.copy(headers = parts.headers.appendHeader(etagHeader, HeaderValue(EntityTagRenderer(etag))))
+    }
+  }
 }
